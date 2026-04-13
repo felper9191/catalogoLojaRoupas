6a7de1b4a6c41356203f25b47c512e2e090531db
felper9191 <84869849+felper9191@users.noreply.github.com>
2026-04-13 22:58:24 +0000

    msg

--- src/Rodape.jsx
+++ src/Rodape.jsx
@@ -3,7 +3,7 @@ import './Rodape.css';
 const Rodape = () => {
     return (
         <footer>
-            <p>Powered by Someone Unknown</p>
+            <p>Powered by Unknown</p>
         </footer>
     );
 };
